dddbc08b8882ddde38457b71195579ff07e5595b
alexa <alexanderkovalyov29@gmail.com>
2023-10-01 21:35:58 +0300

    My homework 3)

---
+++ HW_3/Task_1.py
@@ -0,0 +1,3 @@
+name = "Alex"
+day = "Sunday"
+print("Good day",name,"!",day,"is a perfect day to learn some python.")
---
+++ HW_3/Task_3.py
@@ -0,0 +1,13 @@
+x = 10
+y = 2
+a = 15
+b = 4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x%y)
+print(x//y)
+#Floor division (//) with a&b
+print(a//b)
