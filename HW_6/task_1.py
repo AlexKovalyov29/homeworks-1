e0a70607887494b2a7473a80241fa97c16f05107
alexa <alexanderkovalyov29@gmail.com>
2023-11-14 23:24:25 +0200

    HW_6.1_Corrected

--- HW_6/task_1.py
+++ HW_6/task_1.py
@@ -6,7 +6,6 @@ import random
 list_number = [1,2,3,4,5,6,7,8,9,10]
 #print(type(random.choice(list_number)))
 while random.choice(list_number):
-    if random.choice(list_number) == 10:
+    if max(list_number) == 10:
         print(True)
-    else:
-       print(False)
+        break
